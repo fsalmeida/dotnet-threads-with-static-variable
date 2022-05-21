7e0fb5bf5369df9a6cf69c099d30921aa95c87f9
Felipe Almeida <felipe.silvalmeida@gmail.com>
2022-05-21 00:47:12 -0300

    Cenário multi thread com uso de variável estática dentro da função assíncrona

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotnet_threads_with_static_variable
+{
+    class Program
+    {
+        static double IDADE = 0;
+
+        static async Task Main(string[] args)
+        {
+            IDADE = 30;
+            var taskGetText1 = Task.Run(() => PrintAge("Minha idade é"));
+
+            Thread.Sleep(100);
+            IDADE = 90;
+            var taskGetText2 =
+                Task.Run(() => PrintAge("A idade da minha vó é"));
+
+            await Task.WhenAll(taskGetText1, taskGetText2);
+        }
+
+        static async Task PrintAge(string prefix)
+        {
+            System.Threading.Thread.Sleep(300);
+            Console.WriteLine($"{prefix}: {IDADE}");
+        }
+    }
+}
